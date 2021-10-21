1e3dbc7f28b8a4c41862f4f19026f05ad612e4d9
alexander_erin <sanyaerin@mail.ru>
2021-10-21 10:31:18 +0300

    Небольшой рефакторинг

---
+++ src/Core/Contract/CommandInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SymfonyBundle\UIBundle\Foundation\Core\Contract;
+
+interface CommandInterface
+{
+
+}
--- src/Core/Contract/OutputContractInterface.php
+++ src/Core/Contract/OutputContractInterface.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace SymfonyBundle\UIBundle\Foundation\Core\Contract\Command;
+namespace SymfonyBundle\UIBundle\Foundation\Core\Contract;
 
 interface OutputContractInterface
 {
